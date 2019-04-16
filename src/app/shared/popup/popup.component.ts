9aaadc8ae2e85cc99d89ad232f2575e277458d42
Eduard Duarte <eduard.duarte@hotmail.com>
2019-04-16 17:34:13 -0500

    add @Input()

--- src/app/shared/popup/popup.component.ts
+++ src/app/shared/popup/popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 // import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
 // import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
 // import { ModalComponent } from 'your-path-to-modal-component';
@@ -10,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popup.component.css']
 })
 export class PopupComponent implements OnInit {
+  @Input() headingInput: string;
+  @Input() bodyInput: string;
+  @Input() footerInput: string;
+  heading : string = '';
+  body : string = '';
+  footer : string = '';
 
   constructor() { }
 
   ngOnInit() {
+    this.heading = this.headingInput;
+    this.body = this.bodyInput;
   }
 
 }
